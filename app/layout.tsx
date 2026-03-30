75191c558128ff4b48359299d7835a6a1add7cee
Henning Navrud-Søberg <henning.navrud-soberg@mktv.no>
2026-03-30 14:36:01 +0200

    Lagt til Analytics og Speed Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
   title: 'Avyronis — Vi bygger nettsider som gjør besøkende til kunder',
@@ -19,7 +21,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="no">
-      <body>{children}</body>
+      <body>
+        {children}
+        <SpeedInsights />
+        <Analytics />
+      </body>
     </html>
   )
 }
